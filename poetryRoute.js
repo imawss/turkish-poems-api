4749814c1fe88954e52f693feab69ebe6ecb7268
✦aras <arasarkan9@gmail.com>
2023-03-14 13:22:38 +0300

    route settings

--- poetryRoute.js
+++ poetryRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { randomPoem } from "./controllers/poetryController.js"
+import { randomPoem, randomPoemWithFilter } from "./controllers/poetryController.js"
 
 export const poetryRoute = express.Router();
 
 poetryRoute.get('/random', randomPoem);
+poetryRoute.get('/random/:categoryId', randomPoemWithFilter);
 
 export default poetryRoute;
